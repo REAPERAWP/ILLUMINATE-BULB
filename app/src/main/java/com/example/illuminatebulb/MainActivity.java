36c0afdb8dcb097b097ce0928c5e5101dbe51e8e
Akshay Arebail <34510695+REAPERAWP@users.noreply.github.com>
2020-09-24 12:27:36 +0530

    My first Commit

---
+++ app/src/main/java/com/example/illuminatebulb/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.illuminatebulb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void bluetap(View view) {
+        ImageView blue = (ImageView) findViewById(R.id.bluebulb);
+        ImageView green = (ImageView) findViewById(R.id.greenbulb);
+        blue.animate().alpha(1).setDuration(1000);
+        green.animate().alpha(0).setDuration(1000);
+    }
+    public void greentap(View view) {
+        ImageView blue = (ImageView) findViewById(R.id.bluebulb);
+        ImageView green = (ImageView) findViewById(R.id.greenbulb);
+        blue.animate().alpha(0).setDuration(1000);
+        green.animate().alpha(1).setDuration(1000);
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
